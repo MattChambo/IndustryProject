3169a2ae7b2ca1d9b810777bbd39a844688e854e
matthew william chamberlain <845862784@kent-202-15m.local>
2016-11-09 11:44:29 +1300

    Add hamburger menu

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,10 +6,34 @@
 		<title>Sophisticated Pedagogical Practice</title>
 		<meta name="description" content="Sophisticated Pedagogical Practice">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="/css/styles.css">
 	</head>
 	<body>
 
-		<h1>Sohisticated Pedagogical Practice</h1>
+		<nav>
+			<a href="/"><img src="/images/logo.png" class="logo"></a>
+			<button class="hamburger"><i class="fa fa-bars" aria-hidden="true"></i></button>
+			
+			<ul>
+				<li>
+					<a href="/About">About Mary</a>
+				</li>
+				<li>
+					<a href="/AboutBook">About the Book</a>
+				</li>
+				<li>
+					<a href="/Index">Index</a>
+				</li>
+			</ul>
+			<ul class="loginsignup">
+				<li>
+					<a href="">Login</a> | <a href="">Sign Up</a>
+				</li>
+			</ul>
+			
+		</nav>
 
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+		<script src="/js/navigation.js"></script>
 	</body>
 </html>
--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -8,8 +8,40 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 	</head>
 	<body>
-
+		<nav></nav>
 		<h1>Index Page</h1>
+		<ul>
+			<li>
+				<a href="#">Chapter 1</a>
+			</li>
+			<li>
+				<a href="#">Chapter 2</a>
+			</li>
+			<li>
+				<a href="#">Chapter 3</a>
+			</li>
+			<li>
+				<a href="#">Chapter 4</a>
+			</li>
+			<li>
+				<a href="#">Chapter 5</a>
+			</li>
+			<li>
+				<a href="#">Chapter 6</a>
+			</li>
+			<li>
+				<a href="#">Chapter 7</a>
+			</li>
+			<li>
+				<a href="#">Chapter 8</a>
+			</li>
+			<li>
+				<a href="#">Chapter 9</a>
+			</li>
+			<li>
+				<a href="#">Chapter 10</a>
+			</li>
+		</ul>
 
 	</body>
 </html>
