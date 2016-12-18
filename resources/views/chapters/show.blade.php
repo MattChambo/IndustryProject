ed4eb93c25810025075d56b5b2a01d683d657d9d
matthew william chamberlain <845862784@kent-202-15m.local>
2016-12-19 11:48:56 +1300

    Fix minor error

--- resources/views/chapters/show.blade.php
+++ resources/views/chapters/show.blade.php
@@ -60,7 +60,7 @@
 					{!! $comment->comment !!}
 				</div>
 				<br>
-				@if (Auth::check() && $comment->user_id == Auth::user()->id)
+				@if (Auth::check() && $comment->user_id == Auth::user()->id || Auth::user()->privilege == 'admin')
 					<a href="/EditComment/{{ $comment->id }}" class="btn btn-primary">Edit Comment</a>
 					<button class="btn btn-danger deletetoggle">Delete Comment</button>
 					<a href="/EditComment/destroy/{{ $comment->id }}" class="btn btn-danger btn-md">Yes</a>
